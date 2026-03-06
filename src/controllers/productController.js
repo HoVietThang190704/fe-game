ad61183750fb5dbadeaed2a2b1e5be7cffa67143
Nguyễn Quang Trung <quangtrungnguyeen@gmail.com>
2026-03-06 22:48:21 +0700

    refactor: remove redundant comments and clean up source code

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,5 @@
-/**
- * Product Controller
- * Xử lý logic nghiệp vụ cho tính năng thêm / quản lý sản phẩm
- */
 const productModel = require('../models/productModel');
 
-// [GET] /products - Danh sách sản phẩm
 exports.index = async (req, res) => {
   try {
     const products = await productModel.getAll();
@@ -14,12 +9,10 @@ exports.index = async (req, res) => {
   }
 };
 
-// [GET] /products/add - Form thêm sản phẩm
 exports.showAddForm = async (req, res) => {
   res.render('products/add', { error: null });
 };
 
-// [POST] /products/add - Xử lý thêm sản phẩm
 exports.addProduct = async (req, res) => {
   const { name, price, category_id } = req.body;
   try {
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-// Danh sách sản phẩm
 router.get('/', productController.index);
-
-// Form thêm sản phẩm
 router.get('/add', productController.showAddForm);
-
-// Xử lý POST thêm sản phẩm
 router.post('/add', productController.addProduct);
 
 module.exports = router;
